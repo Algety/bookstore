4d699db9fdb396703ca695792a2d2da2a749ffb7
Algety <algety@ukr.net>
2025-09-06 23:27:06 +0300

    Books app models created, registered in Admin

--- books/admin.py
+++ books/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import Category, BookContributor, Publisher, Book
 
 # Register your models here.
+admin.site.register(Book)
+admin.site.register(Category)
+admin.site.register(BookContributor)
+admin.site.register(Publisher)
