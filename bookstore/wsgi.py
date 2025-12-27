eb7ddf7cc51a6eef075befad1d6ffc6a481549ee
Algety <algety@ukr.net>
2025-12-27 04:31:17 +0200

    wsgi changed

--- bookstore/wsgi.py
+++ bookstore/wsgi.py
@@ -8,16 +8,21 @@ https://docs.djangoproject.com/en/3.2/howto/deployment/wsgi/
 """
 
 import os
-import sys
-
-try:
-    import env
-except ImportError:
-    pass
 
 from django.core.wsgi import get_wsgi_application
 
-sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'bookstore.settings')
 
 application = get_wsgi_application()
+
+# try:
+#     import env
+# except ImportError:
+#     pass
+
+# from django.core.wsgi import get_wsgi_application
+
+# sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+# os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'bookstore.settings')
+
+# application = get_wsgi_application()
