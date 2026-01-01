d589301e384020785c7b9ddc575b7d90d53a6469
Algety <algety@ukr.net>
2026-01-01 23:38:30 +0200

    Multiple select fields widgets added to add_book page

--- books/forms.py
+++ books/forms.py
@@ -1,19 +1,39 @@
 from django import forms
-from .models import Book, Category
+from .models import Book, Category, BookContributor
 
 
 class BookForm(forms.ModelForm):
     class Meta:
         model = Book
         fields = '__all__'
+        widgets = {
+            'categories': forms.CheckboxSelectMultiple(),
+            'authors': forms.CheckboxSelectMultiple(),
+            'illustrators': forms.CheckboxSelectMultiple(),
+        }
 
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
 
         # Filter to show only active categories
-        active_categories = Category.objects.filter(active=True)
+        active_categories = Category.objects.filter(active=True).order_by('parent', 'name')
         self.fields['categories'].queryset = active_categories
+        
+        # Filter authors and illustrators by role
+        self.fields['authors'].queryset = BookContributor.objects.filter(
+            role='author'
+        ).order_by('name')
+        
+        self.fields['illustrators'].queryset = BookContributor.objects.filter(
+            role='illustrator'
+        ).order_by('name')
 
         # Apply consistent styling to all fields
         for field_name, field in self.fields.items():
-            field.widget.attrs['class'] = 'border-black rounded-0'
+            if field_name not in ['categories', 'authors', 'illustrators']:
+                field.widget.attrs['class'] = 'border-black rounded-0'
+
+        # Custom label formatting for categories
+        self.fields['categories'].label_from_instance = lambda obj: (
+            f"{obj.parent.name} / {obj.name}" if obj.parent else obj.name
+        )
