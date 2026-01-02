b33e32d664633f61b898bf8cff84a7d813af02a3
Algety <algety@ukr.net>
2026-01-02 19:36:51 +0200

    Fix add_book path

--- books/urls.py
+++ books/urls.py
@@ -3,6 +3,6 @@ from . import views
 
 urlpatterns = [
     path('', views.all_books, name='books'),
-    path('add/', views.add_book, name='add_book'),
+    path('add_book/', views.add_book, name='add_book'),
     path('<slug:slug>/', views.book_detail, name='book_detail'),
 ]
