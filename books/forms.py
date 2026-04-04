712ac63c338885d4c8c1946989a3ff94ddd9f9cb
Algety <algety@ukr.net>
2026-04-04 10:40:52 +0300

    Adjust button padding on add and edit book pages

--- books/forms.py
+++ books/forms.py
@@ -34,7 +34,7 @@ class BookForm(forms.ModelForm):
         ),
         widget=forms.SelectMultiple(
             attrs={
-                "class": "selectpicker",
+                "class": "selectpicker form-control",
                 "data-live-search": "true",
                 "data-actions-box": "true",
                 "title": "Choose categories...",
@@ -73,7 +73,7 @@ class BookForm(forms.ModelForm):
             "image": CustomClearableFileInput(),
             "authors": forms.SelectMultiple(
                 attrs={
-                    "class": "selectpicker",
+                    "class": "selectpicker form-control",
                     "data-live-search": "true",
                     "data-actions-box": "true",
                     "title": "Choose authors...",
@@ -81,7 +81,7 @@ class BookForm(forms.ModelForm):
             ),
             "illustrators": forms.SelectMultiple(
                 attrs={
-                    "class": "selectpicker",
+                    "class": "selectpicker form-control",
                     "data-live-search": "true",
                     "data-actions-box": "true",
                     "title": "Choose illustrators...",
